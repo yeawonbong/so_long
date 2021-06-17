e30bac01cb62470e58b74b1d1dac1425d61280bb
yeawonbong <ybong.develop@gmail.com>
2021-06-17 22:09:30 +0900

    0617_22h08m_working_on_mlx

--- main.c
+++ main.c
@@ -1,9 +1,10 @@
-#include "./minilibx_mms_20200219/mlx.h"
-#include <stdlib.h>
+#include "so_long.h"
 
 
 typedef struct s_img
 {
+	void	*mlx;
+	void	*window;
 	void	*img;
 	char	*addr;
 	int		bits_per_pixel;
@@ -11,6 +12,19 @@ typedef struct s_img
 	int		endian;
 }			t_img;
 
+int		move_character(int keycode, t_img *img)
+{
+	if (keycode == W)
+	{
+
+	}
+	if (keycode == ESC)
+	{
+		mlx_destroy_window(img->mlx, img->window);
+		exit(0);//정상종료 찾아볼것
+	}
+	return 0;
+}
 
 void	put_pixel(t_img *img, int x, int y, int color)
 {
@@ -22,23 +36,27 @@ void	put_pixel(t_img *img, int x, int y, int color)
 
 int main(void)
 {
-	void 	*mlx;
-	void 	*window;
+
 	t_img	*img;
 
-    mlx = mlx_init();
 	img = malloc(sizeof(t_img));
+    img->mlx = mlx_init();
 	img->bits_per_pixel = 1;
 	img->line_length = 1920;
-    window = mlx_new_window(mlx, 1920, 1080, "test");
-	img->img = mlx_new_image(mlx, 500, 500);
+	img->window = mlx_new_window(img->mlx, 1920, 1080, "test");
+	img->img = mlx_new_image(img->mlx, 192, 108);
 	img->addr = mlx_get_data_addr(img->img, &img->bits_per_pixel, &img->line_length, &img->endian);
 
-	for (int i = 0; i < 100; i++)
+	for (int i = 0; i <= 192; i++)
 	{
-		put_pixel(img, i, i, 0x000000FF);
+		for (int j = 0; j <= 108; j++)
+			put_pixel(img, i, j, 0x000000FF);
 	}
-	mlx_put_image_to_window(mlx, window, img->img, 500, 500);
-	mlx_loop(mlx);
+	for (int size = 0; size <= 10; size++)
+	{
+		mlx_put_image_to_window(img->mlx, img->window, img->img, 192 * size, 0);
+	}
+	mlx_hook(img->window, KEYPRESS, 0, &move_character, img);
+	mlx_loop(img->mlx);
+	return 0;
 }
-
---
+++ so_long.h
@@ -0,0 +1,14 @@
+#ifndef __SO_LONG_H__
+# define __SO_LONG_H__
+
+#include <stdlib.h>
+#include "./minilibx_mms_20200219/mlx.h"
+
+# define KEYPRESS 2
+# define W 13
+# define A 0
+# define S 1
+# define D 2
+# define ESC 53
+
+#endif

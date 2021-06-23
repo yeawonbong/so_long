1ba4f987657cf09b816db4322d95295ec420a777
yeawonbong <ybong.develop@gmail.com>
2021-06-23 20:56:12 +0900

    leak_solved

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: ybong <ybong@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/18 05:29:36 by ybong             #+#    #+#             */
-/*   Updated: 2021/06/23 20:50:59 by ybong            ###   ########.fr       */
+/*   Updated: 2021/06/23 20:55:11 by ybong            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,10 +24,11 @@ int	main(void)
 	while ((gnl = get_next_line(fd, line)) > 0)
 	{
 		printf("line = %s\n", *line);
+		free(*line);
 	}
 	printf("line = %s\n", *line);
 	free(*line);
-	system("leaks a.out > leaks_result_temp; cat leaks_result_temp | grep leaked && rm -rf leaks_result_temp");
+	// system("leaks a.out > leaks_result_temp; cat leaks_result_temp | grep leaked && rm -rf leaks_result_temp");
 	return (0);
 }
 
